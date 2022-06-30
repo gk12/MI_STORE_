42d01af62aec44f9a9be83aebaf4f24ac26d3051
Gaurav <gk44035@gmail.com>
2022-06-30 12:40:58 +0530

    day2_yenav_wo_pre

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import PreNavbar from './components/PreNavbar';
-
+import Navbar from '/components/Navbar';
 
 function App() {
   return (
@@ -8,6 +8,9 @@ function App() {
 
     <div>
       <PreNavbar/>
+    </div>
+    <div>
+      <Navbar/>
     </div>
     </>
   );
