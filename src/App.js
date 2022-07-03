d7eb2b6783734b5ab80d79a9fd45e016afcf9883
Gaurav <gk44035@gmail.com>
2022-07-03 13:14:18 +0530

    day5_heading

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Navbar from './components/Navbar.js';
 import {BrowserRouter as Router} from 'react-router-dom'
 import Slider from "./components/Slider.js"
 import data from "./data/data.json"
-import Banner from './components/Banner.js'
+import Banner from './components/Banner.js';
+import Offers from './components/Offers.js';
+import Heading from './components/Heading.js'
 function App() {
   return (
     
@@ -13,6 +15,13 @@ function App() {
       <PreNavbar/>
       <Navbar/>
       <Slider start={data.banner.start}/>
+      <Offers offer={data.offer}/>
+      <Heading text="STAR PRODUCTS"/>
+      <Heading text="HOT ACCESSORIES"/>
+      <Heading text="PRODUCT REVIEWS"/>
+      <Heading text="VIDEOS"/>
+      <Heading text="IN THE PRESS"/>
+
     </Router>
     
     
