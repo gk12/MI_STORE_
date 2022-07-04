4c8f33513f10b2fa0d320019ced215ea3dc6a343
Gaurav <gk44035@gmail.com>
2022-07-05 01:07:24 +0530

    day7_video

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import VideoCard from "./VideoCard.js"
+import "../styles/Videos.css"
 
-const Videos = () => {
+const Videos = ({videos}) => {
   return (
-    <div>Videos</div>
+    <div className="videos">
+    {
+        videos.map((item,index)=>(
+           <VideoCard  index={index} key={item.image} image={item.image} name={item.name}/>
+        ))
+    }
+</div>
   )
 }
 
