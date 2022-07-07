19c67b1fe0b08fb38c47df2fc4d8a86d607d5f00
Gaurav <gk44035@gmail.com>
2022-07-07 21:36:49 +0530

    day10_update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import "./App.css";
 import PreNavbar from "./components/PreNavbar.js"
 import Navbar from "./components/Navbar.js"
-import {BrowserRouter as Router, Route } from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 import Slider from "./components/Slider.js"
 import data from "./data/data.json"
 import Banner from "./components/Banner.js"
 import Offers from "./components/Offers.js"
 import Heading from "./components/Heading.js"
 import StarProduct from "./components/StarProduct.js"
-import HotAccessoriesMenu  from "./components/HotAccessoriesMenu.js"
-import HotAccessories from "./components/HotAccessories.js"
 import ProductReviews from "./components/ProductReviews.js"
 import Videos from "./components/Videos.js"
 import Footer from "./components/Footer.js"
 import NavOptios from "./components/NavOptios.js"
+
 
 function App() {
   return (
@@ -26,39 +25,10 @@ function App() {
       <Slider start={data.banner.start}/>
       <Offers offer={data.offer}/>
       <Heading text="STAR PRODUCTS"/>
-      <StarProduct starProduct={data.starProduct}/>
-
-      <Heading text="HOT ACCESSORIES"/>
-      <HotAccessoriesMenu />
-      {/* { < HotAccessories/> */}
-   
-      <Route exact path="/music">
-      <HotAccessories music={data.hotAccessories.music} musicCover={data.hotAccessoriesCover.music}  />
-      </Route>
-{/*
-      <Route exact path="/smartDevice">
-       <HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice}  />
-       </Route>
-
-      
-       <Route exact path="/home">
-       <HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home}  />
-       </Route>
-       
-
-       <Route exact path="/lifestyle">
-       <HotAccessories lifeStyle={data.hotAccessories.lifeStyle} lifeStyleCover={data.hotAccessoriesCover.lifeStyle}  />
-       </Route>
-
-       <Route exact path="/mobileAccessories">
-       <HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories}  />
-       </Route> */}
-
+      <StarProduct starProduct={data.starProduct}/>   
        <Heading text="PRODUCT REVIEWS"/>
        <ProductReviews productReviews={data.productReviews}/>
-
        <Heading text="VIDEOS" />
-      
       <Videos videos={data.videos} />
       <Heading  text="IN THE PRESS"/>
       <Banner  banner={data.banner}/>
